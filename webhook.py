0a3cba0d4b5f3b71200112c5e4846fa6bcf2183d
vishhs <vishhs@gmail.com>
2018-08-20 18:27:57 +0530

    chatbot

--- webhook.py
+++ webhook.py
@@ -31,7 +31,7 @@ def makeResponse(req):
     date = parameters.get("date")
     if city is None:
         return None
-    r=requests.get('http://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=06f070197b1f60e55231f8c46658d077')
+    r=requests.get('http://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=b6907d289e10d714a6e88b30761fae22')
     json_object = r.json()
     weather=json_object['list']
     for i in range(0,30):
